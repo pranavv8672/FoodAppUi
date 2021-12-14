0f6e0aa78a8da22b2b98022f9f463b914d91556c
pranavv8672 <crce.8672.it@gmail.com>
2021-12-15 00:24:13 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading'; 
+
+import { 
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
+
+import {useFonts } from 'expo-font';
+import colors from './assets/colors/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Home from './components/Home';
+Icon.loadFont();
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  let[fontsLoaded, error] = useFonts({
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options = {{
+          headerShown: false,
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
